02a869acfb2a079e68e86fb7fb9765df87484f43
Nathan Plamondon <nplamondon@gmail.com>
2012-06-08 01:06:44 -0700

    tiled user-spec'd number of jigs. caught several bad inputs.

--- finjig.py
+++ finjig.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 from svgfig import *
-from math import sqrt
+from math import sqrt, ceil, floor
 
 class FinJig(object):
 	def __init__(self, *args):
@@ -9,25 +9,30 @@ class FinJig(object):
 			if i == 0:
 				self.getParams()
 				return
-		self.numFins = int(args[0])
+		self.numFins = int(floor(float(args[0])))
 		self.body_d = float(args[1])
 		self.body_r = 0.5 * float(self.body_d/2)
 		self.fin_h = float(args[2])
 		self.fin_w = float(args[3])
 	def getParams(self):
+		self.numFins = int(floor(input("Number of fins: ")))
 		print("All units are in mm")
-		self.numFins = int(input("Number of fins: "))
 		self.body_d = float(input("Body diam: "))
 		self.body_r = 0.5 * self.body_d
 		self.fin_h = float(input("Fin height: "))
 		self.fin_w = float(input("Fin width: "))
 		
 def makeJig(n=0, d=0, h=0, w=0, outFile="jig.svg"):
-	numPlates = int(input("Number of plates: "))
+	numPlates = int(floor(float(raw_input("Number of plates: "))))
+	if (numPlates == "") or (numPlates < 1):
+		numPlates = 2
+	print(numPlates)
 	jig = FinJig(n, d, h, w)
+	if jig.numFins < 2 or jig.numFins > 360:
+		print("Number of fins must be between 2 and 360.")
+		sys.exit(1)
 	square = (2 * jig.fin_h + jig.body_d) + 20
 	center = square/2
-	wholeSize = square * 2
 	edge = SVG("rect", cx=0, cy=0, width=square, height=square)
 	body = SVG("circle", cx=center, cy=center, r=jig.body_r)
 	fin_angle = 360 / jig.numFins
@@ -37,10 +42,20 @@ def makeJig(n=0, d=0, h=0, w=0, outFile="jig.svg"):
 		fins.append(SVG("rect", x=-fin_offw, y=jig.body_r, width=jig.fin_w, height=jig.fin_h, transform="rotate({})".format(i * fin_angle)))
 	finset = SVG("g", *fins, transform="translate({},{})".format(center,center))
 	plates = []
-	for i in range(numPlates):
+	xpos = 0
+	ypos = 0
+	numPerRow = ceil(sqrt(numPlates))
+	whole_x = (square + 2) * numPerRow
+	whole_y = (square + 2) * ceil((numPlates / numPerRow))
+	for i in range(1,numPlates+1):
 		rocket = SVG("g", body, finset, transform="rotate({},{},{})".format(fin_angle/2,center,center))
-		plates.append(SVG("g", edge, rocket))
-	whole = canvas(*plates, height="{}mm".format(wholeSize), width="{}mm".format(wholeSize), viewBox="0 0 {} {}".format(wholeSize, wholeSize))
+		plates.append(SVG("g", edge, rocket, transform="translate({},{})".format(xpos, ypos)))
+		if i % numPerRow == 0:
+			xpos = 0
+			ypos += square + 2	
+		else:
+			xpos += square + 2
+	whole = canvas(*plates, height="{}mm".format(whole_y), width="{}mm".format(whole_x), viewBox="0 0 {} {}".format(whole_x, whole_y))
 	whole.save(outFile)
 	whole.inkview()
 
