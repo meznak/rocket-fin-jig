2a1bef35b46187f19abc7d527bfe5efb7987ec3d
Nathan Plamondon <nplamondon@gmail.com>
2016-03-09 15:11:18 -0700

    Begin svgwrite rewrite
    So far, I'm able to generate a frame and the body hole.

--- finjig.py
+++ finjig.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from math import sqrt, ceil, floor
-from svgfig import *
+import svgwrite
+import svgwrite.shapes as shapes
 
 class FinJig(object):
 	def __init__(self, *args):
@@ -22,7 +23,7 @@ class FinJig(object):
 		self.fin_h = float(input("Fin height: "))
 		self.fin_w = float(input("Fin width: "))
 		
-def makeJig(n=0, d=0, h=0, w=0, outFile="jig.svg"):
+def makeJig(n=0, d=0, h=0, w=0, out_file="jig.svg"):
 	numPlates = int(floor(float(raw_input("Number of plates: "))))
 	if (numPlates == "") or (numPlates < 1):
 		numPlates = 2
@@ -30,33 +31,58 @@ def makeJig(n=0, d=0, h=0, w=0, outFile="jig.svg"):
 	if jig.numFins < 2 or jig.numFins > 360:
 		print("Number of fins must be between 2 and 360.")
 		sys.exit(1)
+
+
+        # plate properties
 	square = (2 * jig.fin_h + jig.body_d) + 20
 	center = square/2
-	edge = SVG("rect", cx=0, cy=0, width=square, height=square)
-	body = SVG("circle", cx=center, cy=center, r=jig.body_r)
+	edge = shapes.Rect(size=(square,square), stroke="blue", stroke_width=1, fill="white")
+                
+        # body/fin properties
+	body = shapes.Circle(center=(center, center), r=jig.body_r, stroke="black", stroke_width=1, fill="white")
+        fin_offw = jig.fin_w / 2
+        fin_offh = jig.fin_h
+        fin = shapes.Rect(insert=(center - fin_offw, center - fin_offh), size=(jig.fin_w, jig.fin_h), stroke="black", stroke_width=5, fill="white")
 	fin_angle = 360 / jig.numFins
-	fin_offw = jig.fin_w / 2
-	fins = []
-	for i in range(jig.numFins):
-		fins.append(SVG("rect", x=-fin_offw, y=jig.body_r, width=jig.fin_w, height=jig.fin_h, transform="rotate({})".format(i * fin_angle)))
-	finset = SVG("g", *fins, transform="translate({},{})".format(center,center))
-	plates = []
+	        
+        # build a jig drawing
+        dwg = svgwrite.Drawing()
+        xfm = svgwrite.mixins.Transform
+
+        dwg.add(edge)
+        dwg.add(body)
+
+        xfm.translate(fin, center, center)
+        xfm.translate(fin, -fin_offw, -fin_offh)
+        dwg.add(fin)
+	# for i in range(jig.numFins - 1):
+        #         xfm.rotate(fin, fin_angle, (center, center))
+        #         dwg.add(fin)
+
+                
+        # lay out plates
 	xpos = 0
 	ypos = 0
 	numPerRow = ceil(sqrt(numPlates))
 	whole_x = (square + 2) * numPerRow
 	whole_y = (square + 2) * ceil((numPlates / numPerRow))
-	for i in range(1,numPlates+1):
-		rocket = SVG("g", body, finset, transform="rotate({},{},{})".format(fin_angle/2,center,center))
-		plates.append(SVG("g", edge, rocket, transform="translate({},{})".format(xpos, ypos)))
-		if i % numPerRow == 0:
-			xpos = 0
-			ypos += square + 2	
-		else:
-			xpos += square + 2
-	whole = canvas(*plates, height="{}mm".format(whole_y), width="{}mm".format(whole_x), viewBox="0 0 {} {}".format(whole_x, whole_y))
-	whole.save(outFile)
-	whole.inkview()
+
+        out = svgwrite.Drawing(out_file)
+
+        out.add(dwg)
+        # for i in range(1,numPlates+1):
+
+        #         rocket = SVG("g", body, finset, transform="rotate({},{},{})".format(fin_angle/2,center,center))
+	# 	plates.append(SVG("g", edge, rocket, transform="translate({},{})".format(xpos, ypos)))
+	# 	if i % numPerRow == 0:
+	# 		xpos = 0
+	# 		ypos += square + 2	
+	# 	else:
+	# 		xpos += square + 2
+
+                        
+
+        out.save()
 
 if __name__ == '__main__':
 	import sys
